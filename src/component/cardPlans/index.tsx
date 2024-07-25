def7a37a6a8f005194f971b725f81f7eacd67c7e
lobaojoao <joao.lobao@southsystem.com.br>
2024-07-25 20:42:05 -0300

    feat: added cardPlans component

---
+++ src/component/cardPlans/index.tsx
@@ -0,0 +1,87 @@
+import { Button, Flex, Image, Text } from '@chakra-ui/react'
+import IconCheck from '../../assets/IconCheck.svg'
+import { CardPlansProps } from './types'
+
+export const CardPlans = ({
+  typePlans,
+  price,
+  quantityUser,
+  titleButton,
+  isMoreRecommended = false,
+  onPressCard,
+}: CardPlansProps) => {
+  return (
+    <Flex flexDir="column" w="247px" alignItems="center">
+      {isMoreRecommended && (
+        <Flex
+          mb={-6}
+          py={2}
+          px={3}
+          bgColor="#6BBEC3"
+          zIndex={400}
+          borderRadius="lg"
+        >
+          <Text fontFamily="Poppins" fontWeight="600" color="white">
+            Mais recomendado
+          </Text>
+        </Flex>
+      )}
+      <Flex
+        flexDir="column"
+        borderRadius="2xl"
+        borderWidth={isMoreRecommended ? 3 : 1}
+        w="247px"
+        borderColor={isMoreRecommended ? '#6BBEC3' : '#A1A1AA'}
+        py={8}
+        px={6}
+      >
+        <Flex
+          py={3}
+          px={4}
+          bgColor="#F4F4F5"
+          borderRadius="lg"
+          maxWidth="93px"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text
+            fontWeight="600"
+            fontFamily="Poppins"
+            fontSize="lg"
+            color="#52525B"
+          >
+            {typePlans}
+          </Text>
+        </Flex>
+        <Flex mt={6} gap={1} alignItems="center">
+          <Text
+            fontFamily="Poppins"
+            fontWeight="600"
+            fontSize="3xl"
+            color="#3F3F46"
+          >
+            R$ {price.toFixed(2)}
+          </Text>
+          <Text fontFamily="Poppins" fontSize="lg" color="#3F3F46">
+            /mês
+          </Text>
+        </Flex>
+        <Flex gap={3} alignItems="center" mt={8}>
+          <Image src={IconCheck} alt="Icone de check" />
+          <Text color="#52525B" fontSize="lg">
+            {quantityUser} usuários
+          </Text>
+        </Flex>
+        <Button
+          _hover={{ bgColor: '#6BBEC3' }}
+          onClick={onPressCard}
+          bgColor="#379598"
+          color="white"
+          mt={8}
+        >
+          {titleButton}
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
