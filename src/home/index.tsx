32a86d97ed0406982f1f612ecd8a53207a8811f9
Thiago <thiago380tt@gmail.com>
2024-07-25 22:38:23 -0300

    'feat: creation of the download area component'

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,9 +1,11 @@
+import { DownloadArea } from '../component/downloadArea/index'
 import { Header } from '../component/header'
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <DownloadArea />
     </>
   )
 }
