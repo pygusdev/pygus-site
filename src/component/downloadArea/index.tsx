58fac1eff256b5548789df1d5ee59a6fc7e51280
lobaojoao <joao.lobao@southsystem.com.br>
2024-07-26 10:06:22 -0300

    fix: change color and space

--- src/component/downloadArea/index.tsx
+++ src/component/downloadArea/index.tsx
@@ -1,71 +1,51 @@
-
-import { Box, Text, Image, Button, Stack, Flex } from '@chakra-ui/react';
-import appleStoreSvg from '../../assets/apple-logo.svg'; 
-import googlePlaySvg from '../../assets/playstore.svg'; 
+import { Box, Text, Image, Button, Stack, Flex } from '@chakra-ui/react'
+import appleStoreSvg from '../../assets/apple-logo.svg'
+import googlePlaySvg from '../../assets/playstore.svg'
 import cellPhoneImage from '../../assets/imagecelulares.svg'
-
-
-
 
 export const DownloadArea: React.FC = () => {
   return (
     <Flex
-      display="flex" 
-      flexDirection={{ base: 'column', md: 'row' }} 
-      alignItems="center" 
-      justifyContent="center" 
+      alignItems="center"
+      justifyContent="center"
       px={20}
       py={24}
-
-      bgGradient='linear(to-r, white, white, #DFEFF9, #B9DCF3)'
+      bgGradient="linear(to-r, white, white, #DFEFF9, #B9DCF3)"
     >
-      <Box textAlign={{ base: 'center', md: 'left' }} mr={{ md: 8 }}>
-        <Text 
-          fontSize="184px" 
-          mb={6}
-          color='#F47327'
-        >
+      <Box textAlign="left" mr={8}>
+        <Text fontSize="184px" mb={6} color="#F47327">
           PYGUS
         </Text>
-        <Text 
-          fontWeight="light" 
-          fontSize="32px" 
-          color="gray.600" 
-          mb={14}
-        >
-          Desenvolvido para transformar a fala de crianças com transtorno dos sons da fala.
+        <Text fontWeight="light" fontSize="32px" color="#52525B" mb={14}>
+          Desenvolvido para transformar a fala de crianças com transtorno dos
+          sons da fala.
         </Text>
-        <Stack direction={{ base: 'column', md: 'row' }} spacing={6} align="center" >
-          <Button 
-            variant="outline" 
-            as="a" 
-            href="https://apps.apple.com/br/app/pygus-profissional/id123456789" 
-            target="_blank" 
+        <Stack direction="row" spacing={6} align="center">
+          <Button
+            variant="outline"
+            as="a"
+            href="https://apps.apple.com/br/app/pygus-profissional/id123456789"
+            target="_blank"
             p={6}
-     
           >
-            <Image src={appleStoreSvg} mr={4}/>
+            <Image src={appleStoreSvg} mr={4} alt="Logo apple" />
             <Box>
-              <Text fontSize="xs">
-                Download na
-              </Text>
+              <Text fontSize="xs">Download na</Text>
               <Text fontWeight="medium" fontSize="md">
                 App Store
               </Text>
             </Box>
           </Button>
-          <Button 
-            variant="outline" 
-            as="a" 
-            href="https://play.google.com/store/apps/details?id=com.pygus.profissional" 
+          <Button
+            variant="outline"
+            as="a"
+            href="https://play.google.com/store/apps/details?id=com.pygus.profissional"
             target="_blank"
-            p={6} 
+            p={6}
           >
-            <Image src={googlePlaySvg} mr={4} />
+            <Image src={googlePlaySvg} mr={4} alt="Logo android" />
             <Box>
-              <Text fontSize="xs">
-                Download na
-              </Text>
+              <Text fontSize="xs">Download na</Text>
               <Text fontWeight="medium" fontSize="md">
                 Google Play
               </Text>
@@ -73,14 +53,13 @@ export const DownloadArea: React.FC = () => {
           </Button>
         </Stack>
       </Box>
-      <Image 
-        src= {cellPhoneImage} 
-        alt="Imagem com 3 celulares" 
+      <Image
+        src={cellPhoneImage}
+        alt="Imagem com 3 celulares"
         boxSize="auto"
-        ml={{ md: 8 }}
-        mt={{ base: 4, md: 0 }}
+        ml={8}
+        mt={4}
       />
     </Flex>
-  );
-};
-
+  )
+}
