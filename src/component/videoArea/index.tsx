9432b49f846114ce8f325bd839030e5da6941cd7
lobaojoao <joao.lobao@southsystem.com.br>
2024-07-29 21:28:57 -0300

    feat: added video area

---
+++ src/component/videoArea/index.tsx
@@ -0,0 +1,26 @@
+import { Flex } from '@chakra-ui/react'
+import backgroundImage from '../../assets/backgroundVideo.svg'
+
+export const VideoArea = () => {
+  return (
+    <Flex
+      w="100%"
+      h="1440px"
+      bgImage={backgroundImage}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <iframe
+        width="910"
+        height="511"
+        src="https://www.youtube.com/embed/vbtQdy2dO80?si=qGlFM38626EvdyJO"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+      ></iframe>
+    </Flex>
+  )
+}
